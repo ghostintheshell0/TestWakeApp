7263a072b636ff691c9bd3db40d4d2c1f74ff717
user <user>
2020-11-10 09:41:46 +0300

    Add menu

---
+++ Assets/Code/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public int firstSceneGameIndex = 1;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstSceneGameIndex);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
